d0378be31e681d8aceed7b1d284ef2cad40605bd
ZarakUofT <68486041+ZarakUofT@users.noreply.github.com>
2020-12-21 01:05:02 -0500

    Added Obstacle Avoidance
    Added the additional functionality of adding obstacles to the Canvas by clicking and the boids will avoid those.

--- boid.js
+++ boid.js
@@ -1,3 +1,29 @@
+class Obstacle {
+    constructor(x, y, r) {
+        this.position = createVector(x, y);
+        this.r = r;
+        this.color = [random(30, 255), random(30, 255), random(30, 255)];
+    }
+    draw() {
+        fill(this.color);
+        circle(this.position.x, this.position.y, this.r);
+    }
+
+    intersects(boid) {
+        let v = boid.velocity.copy().normalize().mult(Boid.feeler);
+        // let v = p5.Vector.add(boid.position, boid.velocity.copy().normalize().mult(Boid.feeler))
+
+        let a = p5.Vector.sub(this.position, boid.position);
+        let p = v.copy().mult(v.dot(a) / v.magSq());
+        let b = p5.Vector.sub(a, p);
+
+        if (v.mag() > p.mag() && b.mag() < this.r * 1.2) {
+            return true;
+        } else
+            return false;
+    }
+}
+
 class Boid {
     constructor() {
         this.position = createVector(random(width), random(height));
@@ -9,10 +35,15 @@ class Boid {
 
     //Static Variables
     static maxForce;
+    static maxAvoidForce = 10.0;
     static maxSpeed;
     static viewAngle;
-    static r = 3.0;
+    static r = 2.0;
+    static feeler = 60.0;
 
+    findDistance(obj1, obj2) {
+        return dist(obj1.position.x, obj1.position.y, obj2.position.x, obj2.position.y);
+    }
     edges() {
         if (this.position.x < -Boid.r) this.position.x = width + Boid.r;
         if (this.position.y < -Boid.r) this.position.y = height + Boid.r;
@@ -33,28 +64,6 @@ class Boid {
             let d = dist(this.position.x, this.position.y, other.position.x, other.position.y);
             if (other != this && d < perceptionRadius && this.inView(other)) {
                 steering.add(other.velocity);
-                total++;
-            }
-        }
-        if (total > 0) {
-            steering.div(total);
-            steering.setMag(Boid.maxSpeed);
-            steering.sub(this.velocity);
-            steering.limit(Boid.maxForce);
-        }
-        return steering;
-    }
-
-    seperation(boids) {
-        let perceptionRadius = 50;
-        let steering = createVector();
-        let total = 0;
-        for (let other of boids) {
-            let d = dist(this.position.x, this.position.y, other.position.x, other.position.y);
-            if (other != this && d < perceptionRadius && this.inView(other)) {
-                let diff = p5.Vector.sub(this.position, other.position);
-                diff.div(Math.pow(d, 2));
-                steering.add(diff);
                 total++;
             }
         }
@@ -89,18 +98,70 @@ class Boid {
         return steering;
     }
 
-    flock(boids) {
+    seperation(boids) {
+        let perceptionRadius = 50;
+        let steering = createVector();
+        let total = 0;
+        for (let other of boids) {
+            let d = dist(this.position.x, this.position.y, other.position.x, other.position.y);
+            if (other != this && d < perceptionRadius && this.inView(other)) {
+                let diff = p5.Vector.sub(this.position, other.position);
+                diff.div(Math.pow(d, 2));
+                steering.add(diff);
+                total++;
+            }
+        }
+        if (total > 0) {
+            steering.div(total);
+            steering.setMag(Boid.maxSpeed);
+            steering.sub(this.velocity);
+            steering.limit(Boid.maxForce);
+        }
+        return steering;
+    }
+
+    closestObstacle(obstacles) {
+        let cObs = null;
+
+        for (let obstacle of obstacles) {
+            if (obstacle.intersects(this) && (cObs == null || this.findDistance(this, obstacle) < this.findDistance(this, cObs))) {
+                cObs = obstacle;
+            }
+        }
+        return cObs;
+    }
+    avoidObstacles(obstacles) {
+        let cObs = this.closestObstacle(obstacles);
+        let avoidance = createVector(0);
+        if (cObs != null) {
+            let dVec = p5.Vector.sub(cObs.position, this.position);
+            let dist = dVec.mag();
+            let heading = this.velocity.copy();
+            let angleBet = dVec.angleBetween(heading)
+            if (angleBet > 0)
+                heading.rotate(HALF_PI - angleBet);
+            else
+                heading.rotate(-HALF_PI - angleBet);
+            avoidance = heading.div(dist / Boid.maxAvoidForce)
+            // avoidance.limit(Boid.maxAvoidForce);
+        }
+        return avoidance;
+    }
+
+    flock(boids, obstacles) {
         let alignment = this.align(boids);
         let cohesion = this.cohesion(boids);
         let seperation = this.seperation(boids);
+        let obstacleAvoidance = this.avoidObstacles(obstacles);
 
-        seperation.mult(seperationSlider.value());
-        cohesion.mult(cohesionSlider.value());
         alignment.mult(alignSlider.value());
+        cohesion.mult(cohesionSlider.value());
+        seperation.mult(seperationSlider.value());
 
         this.acceleration.add(seperation);
         this.acceleration.add(alignment);
         this.acceleration.add(cohesion);
+        this.acceleration.add(obstacleAvoidance);
     }
 
     update() {
--- sketch.js
+++ sketch.js
@@ -1,24 +1,32 @@
 const flock = [];
 
+let canvas;
+
 let alignSlider, cohesionSlider, seperationSlider;
 let view_angle_slider, speed_slider, force_slider;
-const flock_size = 100;
+const flock_size = 250;
 const perception = 50;
 
-function initialize_sliders() {
-    alignSlider = createSlider(0, 5, 1, 0.1);
-    alignSlider.position(0.1 * width, 1.2 * height);
-    cohesionSlider = createSlider(0, 5, 1, 0.1);
-    cohesionSlider.position(0.4 * width, 1.2 * height);
-    seperationSlider = createSlider(0, 5, 1.1, 0.1);
-    seperationSlider.position(0.7 * width, 1.2 * height);
+let obstacles = []
+let clickLoc = null
 
-    view_angle_slider = createSlider(60, 180, 90, 20);
-    view_angle_slider.position(0.1 * width, 1.35 * height);
-    speed_slider = createSlider(1, 5, 2, 0.5);
-    speed_slider.position(0.4 * width, 1.35 * height);
-    force_slider = createSlider(0, 0.5, 0.1, 0.1);
-    force_slider.position(0.7 * width, 1.35 * height);
+function initialize_sliders() {
+    let height_loc = 1.35
+    alignSlider = createSlider(0, 5, 1, 0.1);
+    alignSlider.position(0.1 * width, height_loc * height);
+    cohesionSlider = createSlider(0, 5, 1, 0.1);
+    cohesionSlider.position(0.4 * width, height_loc * height);
+    seperationSlider = createSlider(0, 5, 1.1, 0.1);
+    seperationSlider.position(0.7 * width, height_loc * height);
+
+    height_loc = 1.5
+
+    view_angle_slider = createSlider(0, 180, 90, 20);
+    view_angle_slider.position(0.1 * width, height_loc * height);
+    speed_slider = createSlider(0, 5, 2, 0.5);
+    speed_slider.position(0.4 * width, height_loc * height);
+    force_slider = createSlider(0, 0.3, 0.1, 0.1);
+    force_slider.position(0.7 * width, height_loc * height);
 }
 
 function update_slider_vals() {
@@ -28,8 +36,10 @@ function update_slider_vals() {
 }
 
 function setup() {
-    createCanvas(600, 400);
+    canvas = createCanvas(600, 400);
+    canvas.mousePressed(getObstacle);
     initialize_sliders();
+    obstacles.push(new Obstacle(300, 200, 40));
     for (let i = 0; i < flock_size; i++) {
         flock.push(new Boid());
     }
@@ -38,7 +48,7 @@ function setup() {
 function draw() {
     background(0);
     update_slider_vals();
-
+    obstacles.forEach(o => o.draw());
     let boundary = new Rectangle(width / 2, height / 2, width / 2, height / 2);
     qtree = new Quad(boundary, 4);
 
@@ -49,8 +59,22 @@ function draw() {
         let range = new Rectangle(boid.position.x, boid.position.y, perception, perception);
         let others = qtree.query(range);
         boid.edges();
-        boid.flock(others);
+        boid.flock(others, obstacles);
         boid.update();
         boid.show();
     }
+}
+
+// function mousePressed() {
+
+//     // return false to prevent default
+//     return false;
+// }
+
+function getObstacle() {
+    clickLoc = [mouseX, mouseY];
+    obstacle_radius = random(20, 40)
+    obstacles.push(new Obstacle(clickLoc[0], clickLoc[1],obstacle_radius));
+    clickLoc = null;
+    return false;
 }
